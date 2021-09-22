c69305c7bfa8ffd7ef34502e7edf5cdfb3f889c4
ticavin <606740@gmail.com>
2021-09-22 23:30:35 +0300

    task5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://1gm0dvreyf.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://1gm0dvreyf.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://1gm0dvreyf.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://ozgyd5wzgg.execute-api.eu-west-1.amazonaws.com/dev	',
   bff: 'https://1gm0dvreyf.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://1gm0dvreyf.execute-api.eu-west-1.amazonaws.com/dev',
 };
